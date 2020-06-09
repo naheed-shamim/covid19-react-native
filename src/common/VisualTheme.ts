5510a6b5a3a6de16021a518f6b92d5c9ead54935
Naheed <naheeds@mindfiresolutions.com>
2020-06-09 22:23:21 +0530

    Added assets

--- src/common/VisualTheme.ts
+++ src/common/VisualTheme.ts
@@ -4,17 +4,11 @@ export const Theme = {
   PRIMARY_ACCENT: 'blue',
 }
 
-const hasNotch = () => {
-  if (Platform.OS === 'android') {
-    return StatusBar.currentHeight > 24;
-  }
-}
 
 export default StyleSheet.create({
   droidSafeArea: {
     flex: 1,
     backgroundColor: 'white',
-    paddingTop: Platform.OS === 'android' && hasNotch ? 25 : 0,
   },
 })
 
